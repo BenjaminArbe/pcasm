79111deafa0e1b33e6fb5ce42c549f875680c6a7
Benjamin Arbe <fernandoarbe@bol.com.br>
2020-04-26 21:26:35 -0400

    counting bits

---
+++ 13count1.s
@@ -0,0 +1,29 @@
+			.file		"count1.s"
+			.section	.text
+			.func		count
+			.type		count, @function
+# -----------------------------------------------------------------------------
+count:
+# extern "C" int count(int n);
+# Description:	Counts the numbers of bits that are "on" in n.
+# @param:		n	
+# @return:		number of bits "on" in n
+		enter	$0, $0
+		push	%ebx
+
+		movb	$0, %bl			# BL will contain the count
+		mov		$32, %ecx		# ECX = loop counter
+1:		shll	$1, %eax		# shift bit into carry flag
+		jnc		2f				# if CF == 0, skip increase BL
+		incb	%bl
+2:		loop	1b
+		movzx	%bl, %eax		
+		pop		%ebx
+		leave
+		ret
+
+		.endfunc
+		.globl	count
+		.end
+
+		
---
+++ 13drcount.cpp
@@ -0,0 +1,36 @@
+/* file: drcount.cpp
+ * to build:	make X=13count1 D=13drcount L=-lstdc++
+ * to test:		./count1
+ */
+#include <iostream>
+#include <iomanip>
+#include <string>
+
+using std::cout;
+using std::cin;
+using std::hex;
+using std::dec;
+using std::string;
+using std::invalid_argument;
+
+extern "C" int count(int);
+
+int 
+main(int argc, char **argv) {
+	string s; unsigned int n = 0;
+
+	try {
+	cout << "Type a hex number(up to 4 bytes): ";
+	cin >> s;
+	for ( char c : s ) {
+		if ( !isxdigit(c) ) throw invalid_argument("Not a hexadecimal number");
+		n = n*16 + (isdigit(c) ? c - '0' : 
+			(isupper(c) ? 0xa + c - 'A' : 0xa + c - 'a') );
+	}
+	cout << "The count of bits that are \"on\" in " << hex << n
+		<< " are " << dec << count(n) << ".\n";
+	}
+	catch (const invalid_argument& e) {
+		cout << e.what();
+	}
+}
