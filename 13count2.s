deae4e200d9568219371a1c5f32199611ee5be93
Benjamin Arbe <fernandoarbe@bol.com.br>
2020-04-26 21:41:36 -0400

    counting bits #2

---
+++ 13count2.s
@@ -0,0 +1,29 @@
+# to build use 13drcount.cpp
+# make X=13count2 D=13drcount L=-lstdc++
+# to test: ./13count2
+		.file		"count2.s"
+		.section	.text
+		.func		count
+		.type		count, @function
+# -----------------------------------------------------------------------------
+count:
+# Description:	Counts the numbers of bits that are "on" in n.
+# @param:		n	
+# @return:		number of bits "on" in n
+		enter	$0, $0
+		push	%ebx
+
+		movb	$0, %bl		# BL = accumulator
+		mov		$32, %ecx	# ECX = loop counter
+1:		shll	$1, %eax	# shift into carry flag
+		adcb	$0, %bl		# add w/carry flag
+		loop	1b
+		movzx	%bl, %eax
+
+		pop		%ebx
+		leave
+		ret
+
+		.endfunc
+		.globl	count
+		.end
